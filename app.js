2ae500db9e32a48bc65c54ca26e1c6f33aa04a0c
Gaurav shukla <shukla5bhaiya@gmail.com>
2026-04-13 22:09:35 +0530

    updating Ui and analyze logic

--- app.js
+++ app.js
@@ -37,3 +37,69 @@ const skillDB = {
 };
 
 
+/* analyze */
+function analyzeResume(){
+
+  if(!resumeText){
+    alert("Upload resume first");
+    return;
+  }
+
+  document.getElementById("loading").classList.remove("hidden");
+  document.getElementById("result").classList.add("hidden");
+
+  setTimeout(()=>{
+
+    const role = document.getElementById("jobRole").value.toLowerCase();
+
+    let required = [];
+
+    if(role.includes("frontend")) required = skillDB.frontend;
+    else if(role.includes("backend")) required = skillDB.backend;
+    else required = skillDB.fullstack;
+
+    const found = required.filter(skill=> resumeText.includes(skill));
+    const missing = required.filter(skill=> !resumeText.includes(skill));
+
+    const score = Math.round((found.length / required.length)*100);
+
+   
+    document.getElementById("result").classList.remove("hidden");
+    document.getElementById("loading").classList.add("hidden");
+
+    
+    document.getElementById("progress").style.width = score+"%";
+    document.getElementById("scoreText").innerText = score + "% Match";
+
+ 
+    const skillsDiv = document.getElementById("skills");
+    skillsDiv.innerHTML="";
+    found.forEach(s=>{
+      skillsDiv.innerHTML += `<span class="px-3 py-1 bg-white text-black rounded">${s}</span>`;
+    });
+
+   
+    const missingDiv = document.getElementById("missing");
+    missingDiv.innerHTML="";
+    missing.forEach(s=>{
+      missingDiv.innerHTML += `<span class="px-3 py-1 bg-red-500 rounded">${s}</span>`;
+    });
+
+    const sug = document.getElementById("suggestions");
+    sug.innerHTML="";
+
+    if(score < 50){
+      sug.innerHTML += "<li>Add more relevant skills</li>";
+      sug.innerHTML += "<li>Build 2-3 strong projects</li>";
+    }
+
+    if(missing.length){
+      sug.innerHTML += `<li>Learn: ${missing.join(", ")}</li>`;
+    }
+
+    if(score > 80){
+      sug.innerHTML += "<li>Add measurable achievements (numbers)</li>";
+    }
+
+  },1000);
+}
